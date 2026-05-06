1ffc94d0598d554d6cf04e0ca978ada93460086d
Sahisahithya19 <nallamillisahithyasrireddy@gmail.com>
2026-05-06 17:36:15 +0530

    Create queries.sql

---
+++ Week1/Day1/queries.sql
@@ -0,0 +1,59 @@
+-- Q1
+SELECT * FROM Employee;
+
+--Q2
+SELECT name, salary FROM Employee;
+
+--Q3
+SELECT * FROM Employee
+WHERE age > 30;
+
+--Q4
+SELECT name FROM Department;
+
+--Q5
+SELECT e.*
+FROM Employee e
+JOIN Department d
+ON e.department_id = d.department_id
+WHERE d.name = 'IT';
+
+--Q6
+SELECT * FROM Employee
+WHERE name LIKE 'J%';
+
+--Q7
+SELECT * FROM Employee
+WHERE name LIKE '%e';
+
+--Q8
+SELECT * FROM Employee
+WHERE name LIKE '%a%';
+
+--Q9
+SELECT * FROM Employee
+WHERE name LIKE '_________';
+
+--Q10
+SELECT * FROM Employee
+WHERE name LIKE '_o%';
+
+--Q11
+SELECT * FROM Employee
+WHERE YEAR(hire_date) = 2020;
+
+--Q12
+SELECT * FROM Employee
+WHERE MONTH(hire_date) = 1;
+
+--Q13
+SELECT * FROM Employee
+WHERE YEAR(hire_date) < 2019;
+
+--Q14
+SELECT * FROM Employee
+WHERE hire_date >= '2021-03-01';
+
+--Q15
+SELECT * FROM Employee
+WHERE hire_date >= DATE_SUB(CURDATE(), INTERVAL 2 YEAR);
